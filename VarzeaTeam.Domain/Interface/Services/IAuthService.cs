dcf7634fead3247ef755ccd7ab55346cf2ba1456
Gustavo <gustavopereirafacal@gmail.com>
2024-04-26 10:17:10 -0300

    docker-compose

--- VarzeaTeam.Domain/Interface/Services/IAuthService.cs
+++ VarzeaTeam.Domain/Interface/Services/IAuthService.cs
@@ -4,6 +4,16 @@ namespace VarzeaLeague.Domain.Interface.Services;
 
 public interface IAuthService
 {
+    Task<IEnumerable<UserModel>> GetAsync(int page, int pageSize);
+
+    Task<UserModel> GetIdAsync(string Id);
+
+    Task<UserModel> CreateAsync(UserModel addObject);
+
+    Task<UserModel> RemoveAsync(string Id);
+
+    Task<UserModel> UpdateAsync(string Id, UserModel updateObject); 
+
     Task<string> Login(UserModel userLogin);
 
     Task<string> ForgetPassword(string email);
