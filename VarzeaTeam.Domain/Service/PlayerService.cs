b69d9f50297ea5df7dfcbc6d43f5864958f3c0e3
Gustavo Pereira Martins <gustavopereifacal@gmail.com>
2024-07-04 00:22:17 -0300

    refactor: pasta

--- VarzeaTeam.Domain/Service/PlayerService.cs
+++ VarzeaTeam.Domain/Service/PlayerService.cs
@@ -1,4 +1,4 @@
-﻿using VarzeaLeague.Domain.Interface.Dao;
+using VarzeaLeague.Domain.Interface.Dao;
 using VarzeaLeague.Domain.Interface.Services;
 using VarzeaTeam.Domain.Exceptions;
 using VarzeaLeague.Domain.Model;
